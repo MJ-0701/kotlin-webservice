a0200ae279b208a03fbde903d323e8e28e33c4cf
MJ-0701 <jack2718@naver.com>
2022-06-10 16:35:02 +0900

    init

---
+++ src/main/kotlin/com/example/kotlinwebservice/KotlinWebserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinwebservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWebserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinWebserviceApplication>(*args)
+}
