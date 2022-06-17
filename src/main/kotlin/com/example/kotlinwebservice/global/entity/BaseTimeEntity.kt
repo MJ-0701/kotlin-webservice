b658fd01ed0175f94ce35d1c6d4145074118e341
MJ-0701 <jack2718@naver.com>
2022-06-17 18:33:35 +0900

    updatedAt correct

--- src/main/kotlin/com/example/kotlinwebservice/global/entity/BaseTimeEntity.kt
+++ src/main/kotlin/com/example/kotlinwebservice/global/entity/BaseTimeEntity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinwebservice.global.entity
 
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 import javax.persistence.Column
@@ -16,7 +17,7 @@ class BaseTimeEntity (
     @field:Column(nullable = false, updatable = false)
     private var createdAt : LocalDateTime? = null,
 
-    @field:LastModifiedBy
+    @field:LastModifiedDate
     private var updatedAt : LocalDateTime? = null
 
 )
